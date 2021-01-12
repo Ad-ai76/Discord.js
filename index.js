d851efa49edfa7ba5b39fe4cc1dfe99115167022
MEN_JS <76971568+Ad-ai76@users.noreply.github.com>
2021-01-12 16:37:23 -0300

    update index.js

--- index.js
+++ index.js
@@ -6,12 +6,46 @@ const client = new Client({
 const config = require('./config.json')
 const prefix = config.prefix
 const token = config.token
+
 client.commands = new Collection();
 client.aliases = new Collection();
 client.categories = fs.readdirSync("./commands/");
 ["command"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
-}); 
+});
+
+//change client on line 4
+const client = new Client({
+    disableEveryone: true,
+    partials : ["MESSAGE", "CHANNEL", "REACTION"]
+});
+
+//add 2 events
+client.on('messageReactionAdd', async(reaction, user) => {
+    if(reaction.message.partial) await reaction.message.fetch();
+    if(reaction.partial) await reaction.fetch();
+    if(user.bot) return;
+    if(!reaction.message.guild) return;
+    if(reaction.message.id === '<messageid>'){
+        if(reaction.emoji.name === '<emoji>') {
+            await reaction.message.guild.members.cache.get(user.id).roles.add('<roleID>')
+            user.send('You have obtained a role!')
+        }
+    }
+})
+client.on('messageReactionRemove', async(reaction, user) => {
+    if(reaction.message.partial) await reaction.message.fetch();
+    if(reaction.partial) await reaction.fetch();
+    if(user.bot) return;
+    if(!reaction.message.guild) return;
+    if(reaction.message.id === '<messageid>'){
+        if(reaction.emoji.name === '<emoji>') {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove('<roleID>')
+            user.send('One of your roles has been removed!')
+        }
+    }
+})
+ 
 client.on('ready', () => {
     client.user.setActivity(`${prefix}help`)
     console.log(`${client.user.username} ✅`)
